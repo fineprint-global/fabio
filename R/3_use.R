7fb7b585ce84224379cb6cd7f37386dabfd21b3f
Nikolas Kuschnig <nikolas.kuschnig@wu.ac.at>
2019-12-03 18:01:59 +0100

    Wrap up ethanol conversion

--- R/3_use.R
+++ R/3_use.R
@@ -21,62 +21,102 @@ use <- merge(
   use, by = c("item_code", "item"), all.x = TRUE, allow.cartesian = TRUE)
 use[, use := NA_real_]
 
-# Allocate crops that go directly to a process
+# 100% processes
+cat("Allocating crops going directly to a process. Applies to items:\n\t",
+  paste0(unique(use[type == "100%", item]), collapse = "; "),
+  ".\n", sep = "")
 use[type == "100%", `:=`(use = processing, processing = 0)]
 
-# Allocate live animals to slaughtering
-use[type == "slaughtering"]
+# Slaughtering
+cat("Allocating live animals to slaughtering use. Applies to items:\n\t",
+  paste0(unique(use[type == "slaughtering", item]), collapse = "; "),
+  ".\n", sep = "")
+use[type == "slaughtering", `:=`(use = processing, processing = 0)]
 
-# FUCK >
-# Allocate TCF crops
-crop_tcf <- use[type == "TCF"]
 
-# Dirty - don't do this
-prcs <- u(crop_tcf$proc)
-i <- 3
-itms <- u(crop_tcf[proc == prcs[i], item])
-prod <- u(tcf[Process == prcs[i], Prod.Code])
-supply <- use[item_code == prod, production]
+# Crop TCF ----------------------------------------------------------------
 
-# Pseudo, 1 item
-use$use[...] <- supply / tcf
-use[use > processing, use = processing]
-processing <- processing - use
+# # FUCK >
+# # Allocate TCF crops
+# crop_tcf <- use[type == "TCF"]
 
-# 2 item
-tmp <- data.frame(
-  proc = use[area_code == 1 & year == 2005 & item %in% itms, processing],
-  item = use[area_code == 1 & year == 2005 & item %in% itms, item],
-  prod = use[area_code == 1 & year == 2005 & item_code == prod, production])
-# < THIS
+# # Dirty - don't do this
+# prcs <- u(crop_tcf$proc)
+# i <- 3
+# itms <- u(crop_tcf[proc == prcs[i], item])
+# prod <- u(tcf[Process == prcs[i], Prod.Code])
+# supply <- use[item_code == prod, production]
 
-# Allocate crops to ethanol feedstocks
-# We know TCFs and shares for some countries
+# # Pseudo, 1 item
+# use$use[...] <- supply / tcf
+# use[use > processing, use = processing]
+# processing <- processing - use
 
-eth$poten_o <- eth$other * eth$tcf
-eth$poten_p <- eth$processing * eth$tcf
-# Cap below at 0?
+# # 2 item
+# tmp <- data.frame(
+#   proc = use[area_code == 1 & year == 2005 & item %in% itms, processing],
+#   item = use[area_code == 1 & year == 2005 & item %in% itms, item],
+#   prod = use[area_code == 1 & year == 2005 & item_code == prod, production])
+# # < THIS
+
+
+# Ethanol production ------------------------------------------------------
+
+eth_tcf <- fread("inst/tcf_eth.csv")
+
+eth <- cbs[item_code %in% eth_tcf$item_code, ]
+eth <- merge(eth, eth_tcf[area_code == 231, c("item_code", "tcf")],
+  by = "item_code", all.x = TRUE)
+eth <- merge(eth, eth_tcf[, c("area_code", "item_code", "value")],
+  by = c("area_code", "item_code"), all = TRUE)
+
+eth[, `:=`(pot_oth = other * tcf, pot_proc = processing * tcf)]
+
+na_sum <- function(x) {ifelse(all(is.na(x)), NA_real_, sum(x, na.rm = TRUE))}
 eth_total <- eth[,
-  list(poten_o_t = sum(poten_o, na.rm = TRUE),
-       poten_p_t = sum(poten_p, na.rm = TRUE)),
-  by = "area_code"]
-eth <- merge(eth, eth_total, by = "area_code" all.x = TRUE)
+  list(pot_oth_t = na_sum(pot_oth),
+       pot_proc_t = na_sum(pot_proc)),
+  by = c("area_code", "year")]
+eth <- merge(eth, eth_total, by = c("area_code", "year"), all.x = TRUE)
+eth[, `:=`(share_oth = pot_oth / pot_oth_t,
+           share_proc = pot_proc / pot_proc_t)]
+rm(eth_total)
 
-eth[, `:=`(share_o = poten_o / poten_o_t,
-           share_p = poten_p / poten_p_t)]
-# Kick NAs?
-# Merge with CBS ethanol?
-eth$feedstock_o <- eth$production * eth$share_o / eth$tcf
-# cap at other
-# Too little? Use processing use as well
-if(poten_o_t < production)
-  feedstock_p <- (production - poten_o_t) * share_p / tcf
-# cap at processig
+cat("Country-specific TCFs available for:\n\t",
+  paste0(unique(eth_tcf[, area]), collapse = "; "), ".\n", sep = "")
+cat("Using only information for the US and Brazil.\n")
+eth[!area %in% c("United States of America", "Brazil"), value := NA]
+# Overwrite shares
+eth[!is.na(value), `:=`(share_oth = value, share_proc = value)]
+eth[, value := NULL]
+
+# Allocate according to ethanol production
+eth <- merge(eth,
+  cbs[item_code == 2659, .(area_code, year, eth_prod = production)],
+  by = c("area_code", "year"), all.x = TRUE)
+# First use up `other`, then `processing` for ethanol production
+eth[, `:=`(
+  eth_oth = eth_prod * share_oth / tcf,
+  eth_proc = ifelse(eth_prod > pot_oth_t,
+    (eth_prod - pot_oth_t) * share_proc / tcf, 0))]
+# Cap values at available `other` and `processing`
+eth[, `:=`(
+  eth_oth = ifelse(eth_oth > other, other, eth_oth),
+  eth_proc = ifelse(eth_proc > processing, processing, eth_proc))]
+# Reduce `other` and `processing` and allocate ethanol specifics to `use`
+eth[, `:=`(
+  use = eth_oth + eth_proc,
+  other = other - eth_oth, processing = processing - eth_proc
+)]
+
+eth <- eth[!is.na(use), .(area_code, item_code, year, proc_code = "p084",
+  processing_eth = processing, other_eth = other, use_eth = use)]
+use <- merge(use, eth,
+  by = c("area_code", "item_code", "year", "proc_code"), all.x = TRUE)
+use[!is.na(use_eth), `:=`(
+  use = use_eth, other = other_eth, processing = processing_eth)]
+use[, `:=`(use_eth = NULL, other_eth = NULL, processing_eth = NULL)]
+rm(eth)
 
 
-
-
-
-
-
-
+# Feed use ----------------------------------------------------------------
