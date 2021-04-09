13302c87a28cfc67c2b75918a60ff63ae925ca4b
martinbruckner <martin.bruckner@wu.ac.at>
2021-04-09 14:20:22 +0000

    consider losses as own-use

--- R/11_leontief_inverse.R
+++ R/11_leontief_inverse.R
@@ -40,13 +40,24 @@ for(year in years){
 
   adjust <- ifelse(year %in% years_singular, TRUE, FALSE)
 
-  L <- prep_solve(year = year, Z = Z_m[[as.character(year)]],
-                  Y = Y[[as.character(year)]], X = X[, as.character(year)],
+  Z <- Z_m[[as.character(year)]]
+  Yi <- Y[[as.character(year)]]
+  diag(Z) <- diag(Z) + as.numeric(rowSums(Yi[,grepl("losses",colnames(Yi))]))
+  Yi[,grepl("losses",colnames(Yi))] <- 0
+
+  L <- prep_solve(year = year, Z = Z,
+                  Y = Yi, X = X[, as.character(year)],
                   adj_diag = adjust)
   saveRDS(L, paste0("/mnt/nfs_fineprint/tmp/fabio/v2/wood/", year, "_L_mass.rds"))
 
-  L <- prep_solve(year = year, Z = Z_v[[as.character(year)]],
-                  Y = Y[[as.character(year)]], X = X[, as.character(year)],
+
+  Z <- Z_v[[as.character(year)]]
+  Yi <- Y[[as.character(year)]]
+  diag(Z) <- diag(Z) + as.numeric(rowSums(Yi[,grepl("losses",colnames(Yi))]))
+  Yi[,grepl("losses",colnames(Yi))] <- 0
+
+  L <- prep_solve(year = year, Z = Z,
+                  Y = Yi, X = X[, as.character(year)],
                   adj_diag = adjust)
   saveRDS(L, paste0("/mnt/nfs_fineprint/tmp/fabio/v2/wood/", year, "_L_value.rds"))
 
