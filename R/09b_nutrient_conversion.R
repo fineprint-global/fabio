0e03e445f132928641f2b8b3db765e9fee6179f6
martinbruckner <martin.bruckner@wu.ac.at>
2023-02-21 12:46:44 +0000

    finalize nutrient conversion script

--- R/09b_nutrient_conversion.R
+++ R/09b_nutrient_conversion.R
@@ -9,27 +9,21 @@ use <- readRDS("data/mr_use.rds")
 Y <- readRDS("/mnt/nfs_fineprint/tmp/fabio/v1.2/Y.rds")
 io_codes <- fread("/mnt/nfs_fineprint/tmp/fabio/v1.2/io_codes.csv")
 coeff <- fread("inst/nutrient_coefficients.csv")
-io_codes$calorific <- coeff$calorific[match(io_codes$item_code, coeff$item_code)]
+io_codes$calorific <- coeff$kcal_per_kg[match(io_codes$item_code, coeff$item_code)]
 
 # convert into calories
 sup_e <- mclapply(sup, function(x) {
-  out <- x
-  out@x <- out@x * io_codes$calorific[(out@i+1)]
-  # out[!is.finite(out)] <- 0
+  out <- t(t(x) * io_codes$calorific)
   return(out)
 }, mc.cores = 6)
 
 use_e <- mclapply(use, function(x) {
-  out <- x
-  out@x <- out@x * io_codes$calorific[(out@i+1)]
-  # out[!is.finite(out)] <- 0
+  out <- x * io_codes$calorific
   return(out)
 }, mc.cores = 6)
 
 Y_e <- mclapply(Y, function(x) {
-  out <- x
-  out@x <- out@x * io_codes$calorific[(out@i+1)]
-  # out[!is.finite(out)] <- 0
+  out <- x * io_codes$calorific
   return(out)
 }, mc.cores = 6)
 
