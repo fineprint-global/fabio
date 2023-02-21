6873b206573c257761db93b71810014fe50ca9df
martinbruckner <martin.bruckner@wu.ac.at>
2023-02-21 10:56:28 +0000

    script for conversion into calories

---
+++ R/09b_nutrient_conversion.R
@@ -0,0 +1,40 @@
+
+library("Matrix")
+library("parallel")
+library(data.table)
+
+# read data ---
+sup <- readRDS("data/mr_sup_mass.rds")
+use <- readRDS("data/mr_use.rds")
+Y <- readRDS("/mnt/nfs_fineprint/tmp/fabio/v1.2/Y.rds")
+io_codes <- fread("/mnt/nfs_fineprint/tmp/fabio/v1.2/io_codes.csv")
+coeff <- fread("inst/nutrient_coefficients.csv")
+io_codes$calorific <- coeff$calorific[match(io_codes$item_code, coeff$item_code)]
+
+# convert into calories
+sup_e <- mclapply(sup, function(x) {
+  out <- x
+  out@x <- out@x * io_codes$calorific[(out@i+1)]
+  # out[!is.finite(out)] <- 0
+  return(out)
+}, mc.cores = 6)
+
+use_e <- mclapply(use, function(x) {
+  out <- x
+  out@x <- out@x * io_codes$calorific[(out@i+1)]
+  # out[!is.finite(out)] <- 0
+  return(out)
+}, mc.cores = 6)
+
+Y_e <- mclapply(Y, function(x) {
+  out <- x
+  out@x <- out@x * io_codes$calorific[(out@i+1)]
+  # out[!is.finite(out)] <- 0
+  return(out)
+}, mc.cores = 6)
+
+
+# Store converted variables
+saveRDS(sup_e, "/mnt/nfs_fineprint/tmp/fabio/v1.2/cal/sup.rds")
+saveRDS(use_e, "/mnt/nfs_fineprint/tmp/fabio/v1.2/cal/use.rds")
+saveRDS(Y_e, "/mnt/nfs_fineprint/tmp/fabio/v1.2/cal/Y.rds")
