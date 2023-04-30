b40c5625d89138a0f5b6f7c3b7a9ca28fe8a1b62
Kajwan Rasul <kajwan.rasul@ntnu.no>
2023-04-30 14:29:11 +0200

    Made file docstring version independent.

--- python-import/app/fabio_rds_import.py
+++ python-import/app/fabio_rds_import.py
@@ -4,100 +4,11 @@ The Food and Agriculture Biomass Input-Output database
 
 FABIO provides a set of multi-regional physical supply-use and input-output
 tables covering global agriculture and forestry. The work is based on mostly
-freely available data from FAOSTAT, IEA, EIA, and UN Comtrade/BACI. FABIO
-currently covers 191 countries + RoW, 118 processes and 125 commodities for
-1986-2013.
+freely available data from FAOSTAT, IEA, EIA, and UN Comtrade/BACI.
 
 Detailed information on the construction of the database can be found in
 Bruckner et al (2018): www.doi.org/10.1021/acs.est.9b03554. Please refer to
 this paper when citing FABIO.
-
-The database consists of the following main components,
-in compressed .rds format:
-
-# =============================================================================
-# From the FABIO README
-# =============================================================================
-
-Z:
-    the inter-commodity input-output matrix, displaying the relationships of
-    intermediate use of each commodity in the production of each commodity,
-    in physical units (tons). The matrix has 24000 rows and columns
-    (125 commodities x 192 regions), and is available in two versions, based
-    on the method to allocate inputs to outputs in production processes:
-        
-        Z_mass (mass allocation) and Z_value (value allocation).
-        
-    Note that the row sums of the Z matrix (= total intermediate use by
-    commodity) are identical in both versions.
-
-Y:
-    the final demand matrix, denoting the consumption of all 24000 commodities
-    by destination country and final use category. There are six final use
-    categories (yielding 192 x 6 = 1152 columns): 1) food use, 2) other use
-    (non-food), 3) losses, 4) stock addition, 5) balancing, and 6) unspecified.
-
-X: 
-    the total output vector of all 24000 commodities. Total output is equal to
-    the sum of intermediate and final use by commodity.
-
-L: 
-    the Leontief inverse, computed as (I – A)-1, where A is the matrix of input
-    coefficients derived from Z and x. Again, there are two versions, depending
-    on the underlying version of Z (L_mass and L_value). 
-
-E: 
-    environmental extensions for each of the 24000 commodities, including four
-    resource categories: 1) primary biomass extraction (in tons), 2) land use
-    (in hectares), 3) blue water use (in m3)., and 4) green water use (in m3). 
-
-# =============================================================================
-# Own remarks
-# =============================================================================
-    
-# Satellite accounts.
-# land use
-# biomass
-# green water consumption
-# blue water consumption
-"E_rds" = readRDS(sprintf("%s/E.rds",source_folder))
-
-# Total output
-# A column for each year (28 years)
-# 24000 rows
-"X_rds" = readRDS(sprintf("%s/X.rds",source_folder))
-
-# Final demand
-# 28 years (1986 to 2013)
-# 24000 rows and 192 region * 6 final demand categories = 1152 columns
-"Y_rds" = readRDS(sprintf("%s/Y.rds",source_folder))
-
-# Transactions
-# 28 years (1986 to 2013)
-# Each year features a 24000x24000 matrix
-# Monetary transactions
-# io_codes.csv as row and col index
-"Z_value_rds" = readRDS(sprintf("%s/Z_value.rds",source_folder))
-
-# Transactions
-# See Z_value.rds
-# Physical transactions
-"Z_mass_rds" = readRDS(sprintf("%s/Z_mass.rds",source_folder))
-
-# Input-output
-# 125 items * 192 area = 24000 rows
-"io_codes_csv" = read.csv(sprintf("%s/io_codes.csv",source_folder))
-
-# Supply-use
-# 118 items * 192 areas = 22656 rows
-"su_codes_csv" = read.csv(sprintf("%s/su_codes.csv",source_folder))
-
-# 125 items
-"items_csv" = read.csv(sprintf("%s/items.csv",source_folder))
-
-# 192 regions
-"regions_csv" = read.csv(sprintf("%s/regions.csv",source_folder))
-    
 """
 
 import numpy as np
