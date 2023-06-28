86ee07ab3e525f83e135940880aef1b7888bedc3
Jonas Bunsen <83568124+jbnsn@users.noreply.github.com>
2023-06-28 16:59:44 +0200

    Comply with PEP 8 - Style Guide for Python Code
    https://peps.python.org/pep-0008/

--- python-import/main.py
+++ python-import/main.py
@@ -7,7 +7,7 @@ The FABIO database v2 should be placed in the folder data/fabio_v2/fabio_v2/
 (otherwise the value of `fabio_filepath` needs to be changed).
 
 The folder should contain at least the following files:
-    
+
     - E.rds
     - io_codes.csv
     - items.csv
@@ -16,7 +16,7 @@ The folder should contain at least the following files:
     - Y.rds
     - Z_mass.rds
     - Z_value.rds
-    
+
 # For units see the `fabio_items` dataframe
 
 This import script was written by
@@ -24,9 +24,8 @@ jonas.bunsen@tu-berlin.de
 
 """
 
-import os
-
 # Change/set working directory (OPTIONAL)
+# import os
 # os.chdir("C:/Users/Username/Project")
 
 import app.fabio_rds_import as fabio
@@ -36,61 +35,61 @@ import pandas as pd
 # %% FABIO import
 
 if True:
-    
-    fabio_filepath = "/data/fabio_v2/fabio_v2/" # Default (can be changed)
-    fabio_year = "2013" # Default (can be changed)
+
+    fabio_filepath = "/data/fabio_v2/fabio_v2/"  # Default (can be changed)
+    fabio_year = "2013"  # Default (can be changed)
 
     fabio_items = fabio.read(
-        path = fabio_filepath,
-        year = fabio_year,
+        path=fabio_filepath,
+        year=fabio_year,
         ).items
-    
+
     fabio_regions = fabio.read(
-        path = fabio_filepath,
-        year = fabio_year,
+        path=fabio_filepath,
+        year=fabio_year,
         ).regions
-    
+
     fabio_io_codes = fabio.read(
-        path = fabio_filepath,
-        year = fabio_year,
+        path=fabio_filepath,
+        year=fabio_year,
         ).io_codes
-    
+
     fabio_E = fabio.read(
-        path = fabio_filepath,
-        year = fabio_year,
-        ).E() 
-    
+        path=fabio_filepath,
+        year=fabio_year,
+        ).E()
+
     fabio_X = fabio.read(
-        path = fabio_filepath,
-        year = fabio_year,
+        path=fabio_filepath,
+        year=fabio_year,
         ).X()
-    
+
     fabio_Y = fabio.read(
-        path = fabio_filepath,
-        year = fabio_year,
-        ).Y() 
-    
+        path=fabio_filepath,
+        year=fabio_year,
+        ).Y()
+
     fabio_Z_value = fabio.read(
-        path = fabio_filepath,
-        year = fabio_year,
-        ).Z_value() 
-    
+        path=fabio_filepath,
+        year=fabio_year,
+        ).Z_value()
+
     fabio_Z_mass = fabio.read(
-        path = fabio_filepath,
-        year = fabio_year,
-        ).Z_mass() 
-    
+        path=fabio_filepath,
+        year=fabio_year,
+        ).Z_mass()
+
 # %% Some checks
 
 if False:
-    
-    fabio_Z_value_head = fabio_Z_value.iloc[:100,:100]
-    fabio_Z_mass_head = fabio_Z_mass.iloc[:100,:100]
-    
+
+    fabio_Z_value_head = fabio_Z_value.iloc[:100, :100]
+    fabio_Z_mass_head = fabio_Z_mass.iloc[:100, :100]
+
 # %% Comapre x-imported with x-calculated
 
 if False:
-    
+
     fabio_X_calculated = (
         fabio_Y.sum(axis=1)
         + fabio_Z_mass.sum(axis=1)
@@ -103,20 +102,20 @@ if False:
 # %% Calculation
 
 if False:
-     
+
     print("Determining technical coefficients ...")
-    
+
     # Divide and replace np.nan
     # np.nan occurs if 0/0
-    
+
     fabio_A = fabio_Z_mass.divide(
         fabio_X['x']
-        ).replace(np.nan,0)
-    
+        ).replace(np.nan, 0)
+
     print("Determining identity matrix ...")
-    
+
     fabio_I = np.identity(len(fabio_Z_mass))
-    
+
     print("Determining Leontief inverse ...")
     # Takes approximately ten minutes
-    fabio_L = np.linalg.inv(fabio_I - fabio_A) 
+    fabio_L = np.linalg.inv(fabio_I - fabio_A)
