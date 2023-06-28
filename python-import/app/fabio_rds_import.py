b90c16707bcd466ac6d3e8497e1f7c30615f23ec
Jonas Bunsen <83568124+jbnsn@users.noreply.github.com>
2023-06-28 17:17:18 +0200

    Minor improvements and compliance with PEP 8 – Style Guide for Python Code
    https://peps.python.org/pep-0008/

--- python-import/app/fabio_rds_import.py
+++ python-import/app/fabio_rds_import.py
@@ -25,9 +25,9 @@ Z:
     in physical units (tons). The matrix has 24000 rows and columns
     (125 commodities x 192 regions), and is available in two versions, based
     on the method to allocate inputs to outputs in production processes:
-        
+
         Z_mass (mass allocation) and Z_value (value allocation).
-        
+
     Note that the row sums of the Z matrix (= total intermediate use by
     commodity) are identical in both versions.
 
@@ -37,24 +37,24 @@ Y:
     categories (yielding 192 x 6 = 1152 columns): 1) food use, 2) other use
     (non-food), 3) losses, 4) stock addition, 5) balancing, and 6) unspecified.
 
-X: 
+X:
     the total output vector of all 24000 commodities. Total output is equal to
     the sum of intermediate and final use by commodity.
 
-L: 
+L:
     the Leontief inverse, computed as (I – A)-1, where A is the matrix of input
     coefficients derived from Z and x. Again, there are two versions, depending
-    on the underlying version of Z (L_mass and L_value). 
+    on the underlying version of Z (L_mass and L_value).
 
-E: 
+E:
     environmental extensions for each of the 24000 commodities, including four
     resource categories: 1) primary biomass extraction (in tons), 2) land use
-    (in hectares), 3) blue water use (in m3)., and 4) green water use (in m3). 
+    (in hectares), 3) blue water use (in m3)., and 4) green water use (in m3).
 
 # =============================================================================
 # Own remarks
 # =============================================================================
-    
+
 # Satellite accounts.
 # land use
 # biomass
@@ -97,21 +97,22 @@ E:
 
 # 192 regions
 "regions_csv" = read.csv(sprintf("%s/regions.csv",source_folder))
-    
+
 """
 
 import numpy as np
 import pandas as pd
 from scipy import sparse
-from rpy2  import robjects
-        
+from rpy2 import robjects
+
 # =============================================================================
 # Read FABIO v2 database
 # =============================================================================
-        
+
+
 class read():
     """Read/import FABIO v2 database.
-    
+
     Parameters
     ----------
     path : STR, optional
@@ -127,12 +128,12 @@ class read():
     """
 
     def __init__(
-        self,
-        path = "data/",
-        year = 2013
-        ):
+            self,
+            path=".",
+            year=2013
+            ):
         """Init.
-        
+
         Parameters
         ----------
         path : STR, optional
@@ -148,36 +149,36 @@ class read():
         """
         self.path = path
         self.year = year
-        
+
         print(f"{10*'='} Class `read()` {10*'='}")
         print(f"FABIO year {self.year}")
         print(f"FABIO path {self.path}")
-        
+
         # Items
         # 125 items
         read.items = pd.read_csv(
             f"{self.path}/items.csv"
             )
-        
+
         # Regions
         # 192 regions
         read.regions = pd.read_csv(
             f"{self.path}/regions.csv",
-            encoding = "ISO-8859-1"
+            encoding="ISO-8859-1"
             )
-        
+
         # IO-codes
         # 192 regions * 125 items = 24000 codes
         read.io_codes = pd.read_csv(
             f"{self.path}/io_codes.csv"
             )
-        
+
         # SU-codes
         # 192 regions * 118 x = 22656 ???
         read.su_codes = pd.read_csv(
             f"{self.path}/su_codes.csv"
             )
-        
+
     def E(self):
         """
         Import E (satellite accounts).
@@ -189,9 +190,9 @@ class read():
 
         """
         print("Reading E ...")
-        
+
         readRDS = robjects.r['readRDS']
-        
+
         # Read RDS file
         rds_file = readRDS(f"{self.path}/E.rds")
 
@@ -201,13 +202,13 @@ class read():
         # Create index from dict
         df = pd.DataFrame.from_dict(
             {
-                key : np.asarray(rds_file_year.rx2(key))
+                key: np.asarray(rds_file_year.rx2(key))
                 for key in rds_file_year.names
                 }
             )
-        
+
         return df
-    
+
     def X(self):
         """
         Import x (???).
@@ -216,33 +217,33 @@ class read():
         -------
         df : pd.DataFrame()
             Pandas dataframe containing the total output (transactions +
-            final demand). 
+            final demand).
 
         """
         print("Reading X ...")
-        
+
         readRDS = robjects.r['readRDS']
-        
+
         rds_file = readRDS(f"{self.path}/X.rds")
-        
+
         df = pd.DataFrame(np.array(rds_file))
-        
+
         # Year 1986 to 2013 as columns
-        df.columns = list(range(1986,2013+1))
-        
+        df.columns = list(range(1986, 2013+1))
+
         # Select only one year
-        df = df.loc[:,int(self.year)]
-        
+        df = df.loc[:, int(self.year)]
+
         # Turn colname into string
         df.columns = [str(self.year)]
-        
+
         # Add MultiIndex
         df.index = pd.MultiIndex.from_frame(
             self.io_codes
             )
-        
+
         return df
-    
+
     def Y(self):
         """
         Import Y (final demand).
@@ -254,41 +255,41 @@ class read():
 
         """
         print("Reading Y ... ")
-        
+
         readRDS = robjects.r['readRDS']
-        
+
         rds_file = readRDS(f"{self.path}/Y.rds")
-        
+
         # Select year (string, not int)
         rds_year = rds_file.rx2(f"{self.year}")
-        
+
         # Sparse matrix specs
-        data    = rds_year.do_slot('x')   # in R: x@x
-        indices = rds_year.do_slot('i')   # in R: x@i
-        indptr  = rds_year.do_slot('p')   # in R: x@p
-        shape   = rds_year.do_slot('Dim') # in R: x@Dim or dim(x)
-        
+        data = rds_year.do_slot('x')  # in R: x@x
+        indices = rds_year.do_slot('i')  # in R: x@i
+        indptr = rds_year.do_slot('p')  # in R: x@p
+        shape = rds_year.do_slot('Dim')  # in R: x@Dim or dim(x)
+
         # Turn into amtrix
         rds_year_matrix = sparse.csc_matrix(
             (data, indices, indptr),
             tuple(shape)
             )
-        
+
         # Turn into array
         data_array = sparse.csc_matrix.toarray(rds_year_matrix)
-        
-        # Turn to Pandas 
+
+        # Turn to Pandas
         df = pd.DataFrame(data_array)
-        
+
         # Add column index
         df = df.T
-        
+
         col_index = self.regions
-        col_index = ( # Repeat index n-times
+        col_index = (  # Repeat index n-times
             col_index.loc[col_index.index.repeat(6)].reset_index(drop=True)
             )
         col_index["final demand"] = (
-            self.regions.shape[0] # 192 regions
+            self.regions.shape[0]  # 192 regions
             * [
                 "balancing",
                 "food",
@@ -298,21 +299,21 @@ class read():
                 "unspecific"
                 ]
             )
-        
+
         # Add MultiIndex (columns)
         df.index = pd.MultiIndex.from_frame(
             col_index
             )
-        
+
         df = df.T
-        
+
         # Add MultiIndex (rows)
         df.index = pd.MultiIndex.from_frame(
             self.io_codes
             )
-        
+
         return df
-    
+
     def Z_value(self):
         """
         Import transactions matrix.
@@ -324,47 +325,47 @@ class read():
 
         """
         print("Reading Z_value ...")
-        
+
         readRDS = robjects.r['readRDS']
-        
+
         rds_file = readRDS(f"{self.path}/Z_value.rds")
-        
+
         # Select year (string, not int)
         rds_year = rds_file.rx2(f"{self.year}")
-        
+
         # Sparse matrix specs
-        data    = rds_year.do_slot('x')   # in R: x@x
-        indices = rds_year.do_slot('i')   # in R: x@i
-        indptr  = rds_year.do_slot('p')   # in R: x@p
-        shape   = rds_year.do_slot('Dim') # in R: x@Dim or dim(x)
-        
+        data = rds_year.do_slot('x')  # in R: x@x
+        indices = rds_year.do_slot('i')  # in R: x@i
+        indptr = rds_year.do_slot('p')  # in R: x@p
+        shape = rds_year.do_slot('Dim')  # in R: x@Dim or dim(x)
+
         # Turn into matrix
         rds_year_matrix = sparse.csc_matrix(
             (data, indices, indptr),
             tuple(shape)
             )
-        
+
         # Turn into array
         data_array = sparse.csc_matrix.toarray(rds_year_matrix)
-        
+
         # Turn into pd.DataFrame
-        
+
         df = pd.DataFrame(data_array)
-        
+
         # Add MultiIndex (columns)
         df = df.T
         df.index = pd.MultiIndex.from_frame(
             self.io_codes
             )
         df = df.T
-        
+
         # Add MultiIndex (rows)
         df.index = pd.MultiIndex.from_frame(
             self.io_codes
             )
-        
+
         return df
-        
+
     def Z_mass(self):
         """
         Import transactions matrix.
@@ -376,43 +377,43 @@ class read():
 
         """
         print("Reading Z_mass ...")
-        
+
         readRDS = robjects.r['readRDS']
-        
+
         rds_file = readRDS(f"{self.path}/Z_mass.rds")
-        
+
         # Select year (string, not int)
         rds_year = rds_file.rx2(f"{self.year}")
-        
+
         # Sparse matrix specs
-        data    = rds_year.do_slot('x')   # in R: x@x
-        indices = rds_year.do_slot('i')   # in R: x@i
-        indptr  = rds_year.do_slot('p')   # in R: x@p
-        shape   = rds_year.do_slot('Dim') # in R: x@Dim or dim(x)
-        
+        data = rds_year.do_slot('x')  # in R: x@x
+        indices = rds_year.do_slot('i')  # in R: x@i
+        indptr = rds_year.do_slot('p')  # in R: x@p
+        shape = rds_year.do_slot('Dim')  # in R: x@Dim or dim(x)
+
         # Turn into matrix
         rds_year_matrix = sparse.csc_matrix(
             (data, indices, indptr),
             tuple(shape)
             )
-        
+
         # Turn into array
         data_array = sparse.csc_matrix.toarray(rds_year_matrix)
-        
+
         # Turn into pd.DataFrame
-        
+
         df = pd.DataFrame(data_array)
-        
+
         # Add MultiIndex (columns)
         df = df.T
         df.index = pd.MultiIndex.from_frame(
             self.io_codes
             )
         df = df.T
-        
+
         # Add MultiIndex (rows)
         df.index = pd.MultiIndex.from_frame(
             self.io_codes
             )
-        
+
         return df
