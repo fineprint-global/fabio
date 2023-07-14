65bbe0aeea1ab4e9e873fce57f2572633811170d
martinbruckner <martin.bruckner@wu.ac.at>
2023-07-14 12:40:47 +0000

    add fd_codes and correction for Chinese veg. oils

--- R/00_codes.R
+++ R/00_codes.R
@@ -10,6 +10,8 @@ processes <- unique(processes[,1:2])
 nrreg <- nrow(regions[cbs==TRUE])
 nrcom <- nrow(items)
 nrproc <- nrow(processes)
+fd <- c("balancing", "food", "losses", "other", "processing", "residuals", "stock_addition", "tourist", "unspecified")
+nrfd <- length(fd)
 
 
 io_codes <- data.table(area_code = rep(regions[cbs==TRUE, code], each = nrcom),
@@ -25,8 +27,13 @@ su_codes <- data.table(area_code = rep(regions[cbs==TRUE, code], each = nrproc),
   proc_code = rep(processes$proc_code, nrreg),
   proc = rep(processes$proc, nrreg))
 
+fd_codes <- data.table(area_code = rep(regions[cbs==TRUE, code], each = nrfd),
+  area = rep(regions[cbs==TRUE, name], each = nrfd),
+  fd = rep(fd, nrreg))
+
 fwrite(io_codes, file="/mnt/nfs_fineprint/tmp/fabio/v1.2/io_codes.csv")
 fwrite(su_codes, file="/mnt/nfs_fineprint/tmp/fabio/v1.2/su_codes.csv")
+fwrite(fd_codes, file="/mnt/nfs_fineprint/tmp/fabio/v1.2/fd_codes.csv")
 fwrite(items[, .(comm_code, item_code, item, comm_group, unit, moisture)],
        file="/mnt/nfs_fineprint/tmp/fabio/v1.2/items.csv")
 fwrite(regions[cbs==TRUE, .(iso3c, area_code = code, area = name, region, EU27)],
--- R/13_ex_post_corrections.R
+++ R/13_ex_post_corrections.R
@@ -7,7 +7,43 @@ items <- fread("inst/items_full.csv")
 regions <- fread("inst/regions_full.csv")
 io <- fread("/mnt/nfs_fineprint/tmp/fabio/v1.2/io_codes.csv")
 su <- fread("/mnt/nfs_fineprint/tmp/fabio/v1.2/su_codes.csv")
+fd <- fread("/mnt/nfs_fineprint/tmp/fabio/v1.2/fd_codes.csv")
 Y <- readRDS("/mnt/nfs_fineprint/tmp/fabio/v1.2/Y.rds")
-Zm <- readRDS("/mnt/nfs_fineprint/tmp/fabio/v1.2/Z_mass.rds")
-Zv <- readRDS("/mnt/nfs_fineprint/tmp/fabio/v1.2/Z_value.rds")
 
+# Chinese edible oil statistics
+# Source: China National Grain & Oils Information Center, Comprehensive balance analysis of China's edible oil market, http://www.grainoil.com.cn/, accessed on 01/03/2023
+oil <- fread("inst/oils_china.csv")
+
+years <- names(Y)
+Y_new <- Y
+
+# correct food and other use of veg. oils for China
+i = 1
+for(i in seq_along(Y)){
+  print(years[i])
+  if(as.numeric(years[i]) < 2013){
+    data <- merge(io, oil[year==2013, .(comm_code, food_share)], by = "comm_code", all.x = TRUE, sort = FALSE)
+  } else {
+    data <- merge(io, oil[year==as.numeric(years[i]), .(comm_code, food_share)], by = "comm_code", all.x = TRUE, sort = FALSE)
+  }
+  data <- cbind(data, as.matrix(Y[[i]][,fd$area=="China, mainland"]))
+  # data[, food_share_fao := `41_food` / (`41_food` + `41_other`)]
+  data[, `:=`(food = round((`41_food` + `41_other`) * food_share),
+              other = round((`41_food` + `41_other`) * (1-food_share)))]
+  Y_new[[i]][, fd$area_code==41 & fd$fd=="food"] <- data$food
+  Y_new[[i]][, fd$area_code==41 & fd$fd=="other"] <- data$other
+}
+
+# compare old and new values
+for(i in seq_along(Y)){
+  food <- sum(Y[[i]][io$comm_code %in% oil$comm_code, fd$area_code==41 & fd$fd=="food"])
+  other <- sum(Y[[i]][io$comm_code %in% oil$comm_code, fd$area_code==41 & fd$fd=="other"])
+  share <- food / (food + other)
+  food_new <- sum(Y_new[[i]][io$comm_code %in% oil$comm_code, fd$area_code==41 & fd$fd=="food"])
+  other_new <- sum(Y_new[[i]][io$comm_code %in% oil$comm_code, fd$area_code==41 & fd$fd=="other"])
+  share_new <- food_new / (food_new + other_new)
+  print(paste0(years[i], ": ", round(food/1000000), "/", round(other/1000000), " Mt, ", round(share*100), "% // ",
+               round(food_new/1000000), "/", round(other_new/1000000), " Mt, ", round(share_new*100), "%"))
+}
+
+saveRDS(Y_new, "/mnt/nfs_fineprint/tmp/fabio/v1.2/Y.rds")
